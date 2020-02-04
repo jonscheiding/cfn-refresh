9e5a23ab8c12c42e173da3fd798012741b93fd85
Jonathan Scheiding <jscheiding@certicasolutions.com>
2020-02-04 14:36:53 -0500

    Actually updating cfn stack

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const AWS = require('aws-sdk');
 const yargs = require('yargs');
 
 const args = yargs
@@ -9,12 +11,94 @@ const args = yargs
     alias: 'f',
     describe: 'A YML or JSON file containing a new CloudFormation template.'
   })
+  .option('capability-iam', {
+    boolean: true,
+    describe: 'Acknowledge that the template may create IAM resources.'
+  })
+  .option('capability-named-iam', {
+    boolean: true,
+    describe: 'Acknowledge that the template may create named IAM resources.'
+  })
   .demandOption('stack-name')
   .argv;
 
-console.log(`Updating stack '${args['stack-name']}'.`);
-if(args['template-file']) {
-  console.log(`Will use template from '${args['template-file']}'.`);
-} else {
-  console.log('Will re-use existing template.');
+function delay(time) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
 }
+
+async function execute() {
+  const cfn = new AWS.CloudFormation();
+
+  const capabilities = Object.keys(args)
+    .filter(arg => arg.startsWith('capability-'))
+    .filter(arg => args[arg])
+    .map(arg => arg.replace(/-/g, '_').toUpperCase());
+
+  let templateBody = null;
+  if(args['template-file']) {
+    templateBody = fs.readFileSync(args['template-file']).toString();
+  }
+
+  console.log(`Getting previous parameter values for stack '${args['stack-name']}'.`)
+
+  let describeResult = await cfn
+    .describeStacks({
+      StackName: args['stack-name']
+    })
+    .promise();
+
+  const parameters = describeResult.Stacks[0].Parameters;
+
+  console.log(`Updating stack '${args['stack-name']}'.`);
+
+  try {
+    await cfn
+      .updateStack({
+        StackName: args['stack-name'],
+        Parameters: parameters,
+        UsePreviousTemplate: templateBody === null,
+        TemplateBody: templateBody,
+        Capabilities: capabilities
+      })
+      .promise();
+  } catch(e) {
+    switch(e.code) {
+      case 'InsufficientCapabilitiesException':
+        console.error(`Stack update failed due to missing capabilities. ${e.message}.`)
+        console.error('Use the various --capability-* arguments to provide them.');
+        break;
+      case 'ValidationError':
+        console.error(e.message);
+        break;
+      default:
+        console.error(e);
+        break;
+    }
+  }
+
+  let complete = false;
+
+  do {
+    describeResult = await cfn
+      .describeStacks({
+        StackName: args['stack-name']
+      })
+      .promise();
+
+    const status = describeResult.Stacks[0].StackStatus;
+    console.log(status);
+    complete = status === 'UPDATE_COMPLETE' || status === 'UPDATE_ROLLBACK_COMPLETE'
+
+    if(!complete) {
+      await delay(5000);
+    }
+  } while(!complete);
+
+  for(const o of describeResult.Stacks[0].Outputs) {
+    console.log(`${o.ExportName} : ${o.OutputValue}`);
+  }
+}
+
+execute();
